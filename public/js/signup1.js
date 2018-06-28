dcaff2cb11813ca55be1b3b4decf776680d6119f
akat32 <baduk32127@gmail.com>
2018-06-28 14:49:00 +0900

    params chk

--- public/js/signup1.js
+++ public/js/signup1.js
@@ -3,5 +3,8 @@ function back(){
 }
 
 function next(){
-  location.href = "/signup2"
+  var name = $('#inputEmail').val()
+  var phone = $('#inputPasswd').val()
+  if (name === "" || phone === "") alert("전부 입력해 주세요")
+  else location.href = "/signup2"
 }
--- public/js/signup2.js
+++ public/js/signup2.js
@@ -1,6 +1,10 @@
 function next(){
-  location.href = "/signup3"
+  var name = $('#inputName').val()
+  var phone = $('#inputPhone').val()
+  if (name === "" || phone === "") alert("전부 입력해 주세요")
+  else location.href = "/signup3"
 }
+
 
 function back(){
   history.back()
