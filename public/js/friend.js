c25657f4a1267f70c71e262831893326bbae2acc
akat32 <baduk32127@gmail.com>
2018-07-01 03:42:59 +0900

    add List

---
+++ public/js/friend.js
@@ -0,0 +1,14 @@
+window.onload = function(){
+  var example1 = new Vue({
+    el: '#friend',
+    data: {
+      items: [
+        { message: 'Foo' },
+        { message: 'Bar' },
+        { message: 'Bar' },
+        { message: 'Bar' },
+        { message: 'Bar' }
+      ]
+    }
+  })
+}
