89324445e090047afa7573b3a0573335559e30d3
akat32 <baduk32127@gmail.com>
2018-07-01 22:06:55 +0900

    fix img link

--- public/js/friend.js
+++ public/js/friend.js
@@ -3,11 +3,11 @@ window.onload = function(){
     el: '#friend',
     data: {
       items: [
-        { name: 'Luda' , email : "Luda1@naver.com", img:"http://localhost:3030/img/1.jpg"},
-        { name: 'luda' , email : "Luda2@naver.com", img:"http://localhost:3030/img/2.jpg"},
-        { name: '루다' , email : "Luda3@naver.com", img:"http://localhost:3030/img/3.jpg"},
-        { name: 'Luda' , email : "Luda4@naver.com", img:"http://localhost:3030/img/4.jpg"},
-        { name: '루다' , email : "Luda5@naver.com", img:"http://localhost:3030/img/5.jpg"}
+        { name: 'Luda' , email : "Luda1@naver.com", img:"http://iwin247.info:3030/img/1.jpg"},
+        { name: 'luda' , email : "Luda2@naver.com", img:"http://iwin247.info:3030/img/2.jpg"},
+        { name: '루다' , email : "Luda3@naver.com", img:"http://iwin247.info:3030/img/3.jpg"},
+        { name: 'Luda' , email : "Luda4@naver.com", img:"http://iwin247.info:3030/img/4.jpg"},
+        { name: '루다' , email : "Luda5@naver.com", img:"http://iwin247.info:3030/img/5.jpg"}
       ]
     }
   })
