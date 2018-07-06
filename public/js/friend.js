c5797c35db1cc26d1e05ca34fab0549f7de0ddfa
akat32 <baduk32127@gmail.com>
2018-07-06 20:22:26 +0900

    something

--- public/js/friend.js
+++ public/js/friend.js
@@ -3,14 +3,14 @@ window.onload = function(){
     el: '#friend',
     data: {
       items: [
-        { name: 'Luda' , email : "Luda1@naver.com", img:"http://iwin247.info:3030/img/1.jpg"},
-        { name: 'luda' , email : "Luda2@naver.com", img:"http://iwin247.info:3030/img/2.jpg"},
-        { name: '루다' , email : "Luda3@naver.com", img:"http://iwin247.info:3030/img/3.jpg"},
-        { name: 'Luda' , email : "Luda4@naver.com", img:"http://iwin247.info:3030/img/4.jpg"},
-        { name: '루다' , email : "Luda5@naver.com", img:"http://iwin247.info:3030/img/5.jpg"}
+        { name: 'Luda' , email : "Luda1@naver.com", img:"img/1.jpg"},
+        { name: 'luda' , email : "Luda2@naver.com", img:"img/2.jpg"},
+        { name: '루다' , email : "Luda3@naver.com", img:"img/3.jpg"},
+        { name: 'Luda' , email : "Luda4@naver.com", img:"img/4.jpg"},
+        { name: '루다' , email : "Luda5@naver.com", img:"img/5.jpg"}
       ]
     },
-    methods: {
+    methods:{
       goChat: function(email){
         var link = '/chat/'+email;
         location.href = link
