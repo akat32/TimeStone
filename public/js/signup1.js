d1949a1d46ff964b1d8d87c150430b7575896138
akat32 <baduk32127@gmail.com>
2018-07-08 04:00:14 +0900

    fix reg

--- public/js/signup1.js
+++ public/js/signup1.js
@@ -3,11 +3,13 @@ function back(){
 }
 
 function next(){
-  var email = $('#inputEmail').val()
-  var exptext = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
-  var passwd = $('#inputPasswd').val()
-  var result = exptext.test(email);
-  if (passwd === "" || email === "") return alert("전부 입력해 주세요")
-  if(!result) return alert('이메일 값이 틀렸습니다!')
-  else if (result) location.href = "/signup2"
+  // var email = $('#inputEmail').val()
+  // var exptext = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+  // var passwd = $('#inputPasswd').val()
+  // var result = exptext.test(email);
+  // if (passwd === "" || email === "") return alert("전부 입력해 주세요")
+  // if(!result) return alert('이메일 값이 틀렸습니다!')
+  // else if (result) location.href = "/signup2"
+  location.href = "/signup2"
+  return false;
 }
